76cebfd220417cd65b659e05198857e9fd1a3e2c
Ilkka Kellokoski <ilkka.kellokoski@gmail.com>
2016-10-23 10:46:03 +0300

    Query movie details separately from the movie lists
    The TMDB API restricts queries per API key to 40 queries per 10 seconds

--- app/src/main/java/com/blastbet/nanodegree/popularmovies/sync/MovieSyncAdapter.java
+++ app/src/main/java/com/blastbet/nanodegree/popularmovies/sync/MovieSyncAdapter.java
@@ -9,6 +9,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.SyncRequest;
 import android.content.SyncResult;
+import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -29,6 +30,8 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Vector;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -37,6 +40,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by ilkka on 19.8.2016.
  */
 public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public static final String MOVIE_SYNC_EXTRAS_MOVIE_ID = "movie_sync_id";
+
+    private static final int MOVIE_SYNC_TYPE_GET_DETAILS = 1;
+    private static final int MOVIE_SYNC_TYPE_LIST        = 2;
 
     private static final String LOG_TAG = MovieSyncAdapter.class.getSimpleName();
 
@@ -55,39 +63,82 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
                 context.getString(R.string.pref_sort_by_key),
                 context.getString(R.string.pref_sort_by_default));
 
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        // set your desired log level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        // add your other interceptors …
+        // add logging as last interceptor
+        httpClient.addInterceptor(logging);  // <-- this is the important line!
+
         mRetrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(MOVIEDB_BASE_URL)
+                .client(httpClient.build())
                 .build();
 
         mMovieApi = mRetrofit.create(MovieApi.class);
     }
 
-    @Override
-    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
-        final String sortKey = PreferenceManager.getDefaultSharedPreferences(getContext()).getString(
-                getContext().getString(R.string.pref_sort_by_key),
-                getContext().getString(R.string.pref_sort_by_default));
-
-
-        final MovieList movies = fetchMovies(sortKey);
-        updateMoviesList(movies, sortKey);
+    private String getSortKey() {
+        return PreferenceManager.getDefaultSharedPreferences(getContext())
+                .getString(
+                        getContext().getString(R.string.pref_sort_by_key),
+                        getContext().getString(R.string.pref_sort_by_default)
+                );
     }
 
-    private void fetchMovieDetails(Movie movie) {
+    @Override
+    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
+        int syncType;
+        long movieId = 0L;
+
+        if (bundle.containsKey(MOVIE_SYNC_EXTRAS_MOVIE_ID)) {
+            syncType = MOVIE_SYNC_TYPE_GET_DETAILS;
+            movieId = bundle.getLong(MOVIE_SYNC_EXTRAS_MOVIE_ID);
+        } else {
+            syncType = MOVIE_SYNC_TYPE_LIST;
+        }
+
+        StringBuilder logEntryBuilder = new StringBuilder("Performing sync with extra bundle:\n");
+        for (String key : bundle.keySet()) {
+            logEntryBuilder.append('\t');
+            logEntryBuilder.append(key);
+            logEntryBuilder.append(": ");
+            logEntryBuilder.append(bundle.get(key).toString());
+            logEntryBuilder.append('\n');
+        }
+        Log.v(LOG_TAG, logEntryBuilder.toString());
+
+        switch (syncType) {
+            case MOVIE_SYNC_TYPE_LIST:
+                final String sortKey = getSortKey();
+                final MovieList movies = fetchMovies(sortKey);
+                updateMoviesList(movies, sortKey);
+                break;
+            case MOVIE_SYNC_TYPE_GET_DETAILS:
+                final Movie movie = fetchMovieDetails(movieId);
+                updateMovieDetails(movie);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown movie sync type: " + syncType);
+        }
+    }
+
+    private Movie fetchMovieDetails(long movieId) {
         Call<Movie> movieCall = mMovieApi.getMovieDetails(
-                movie.getId(), BuildConfig.THEMOVIEDB_API_KEY
+                movieId, BuildConfig.THEMOVIEDB_API_KEY
         );
         Call<MovieReviewList> reviewCall = mMovieApi.getMovieReviews(
-                movie.getId(), BuildConfig.THEMOVIEDB_API_KEY
+                movieId, BuildConfig.THEMOVIEDB_API_KEY
         );
         Call<MovieTrailerList> trailerCall = mMovieApi.getMovieTrailers(
-                movie.getId(), BuildConfig.THEMOVIEDB_API_KEY
+                movieId, BuildConfig.THEMOVIEDB_API_KEY
         );
 
+        Movie movie = null;
         try {
-            final Movie detailedMovie = movieCall.execute().body();
-            movie.setRuntime(detailedMovie.getRuntime());
+            movie = movieCall.execute().body();
             final List<MovieReview> movieReviews = reviewCall.execute().body().getMovieReviews();
             movie.setReviews(movieReviews);
             final List<MovieTrailer> movieTrailers = trailerCall.execute().body().getMovieTrailers();
@@ -95,6 +146,8 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return movie;
     }
 
     private MovieList fetchMovies(String sortKey) {
@@ -107,9 +160,6 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         }
 
         List<Movie> movies = movieList.getMovieList();
-        for (Movie movie : movies) {
-            fetchMovieDetails(movie);
-        }
 
         return movieList;
     }
@@ -156,28 +206,37 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         }
     }
 
+    private void updateMovieDetails(final Movie movie) {
+        // Insert movie first
+        ContentResolver resolver = getContext().getContentResolver();
+        ContentValues movieValues = movieToDetailedContentValues(movie);
+        resolver.insert(MovieContract.MovieEntry.CONTENT_URI, movieValues);
+
+        // ...then trailers
+        Vector<ContentValues> trailerVector = new Vector<>(movie.getTrailers().size());
+        addTrailersToContentValuesVector(movie.getTrailers(), trailerVector);
+        ContentValues trailersArray[] = new ContentValues[trailerVector.size()];
+        trailerVector.toArray(trailersArray);
+        int inserted = resolver.bulkInsert(MovieContract.TrailerEntry.CONTENT_URI, trailersArray);
+        Log.d(LOG_TAG, "Trailers sync finished, inserted " + inserted + " rows to trailers table.");
+
+        // ...and finally reviews
+        Vector<ContentValues> reviewVector = new Vector<>(movie.getReviews().size());
+        addReviewsToContentValuesVector(movie.getReviews(), reviewVector);
+        ContentValues reviewsArray[] = new ContentValues[reviewVector.size()];
+        reviewVector.toArray(reviewsArray);
+        inserted = resolver.bulkInsert(MovieContract.ReviewEntry.CONTENT_URI, reviewsArray);
+        Log.d(LOG_TAG, "Reviews sync finished, inserted " + inserted + " rows to reviews table.");
+    }
+
     private void updateMoviesList(final MovieList movies, final String sortKey) {
         List<Movie> movieList = movies.getMovieList();
         Vector<ContentValues> fullDetailVector = new Vector<ContentValues>(movieList.size());
         Vector<ContentValues> movieIdVector = new Vector<ContentValues>(movieList.size());
 
-        int numTrailers = 0;
-        int numReviews = 0;
-
-        for (Movie m : movieList) {
-            numTrailers += m.getTrailers().size();
-            numReviews += m.getReviews().size();
-        }
-
-        Vector<ContentValues> trailerVector = new Vector<>(numTrailers);
-        Vector<ContentValues> reviewVector = new Vector<>(numReviews);
-
         for(Movie movie: movieList) {
             fullDetailVector.add(movieToDetailedContentValues(movie));
             movieIdVector.add(movieToPlainIdContentValues(movie));
-
-            addTrailersToContentValuesVector(movie.getTrailers(), trailerVector);
-            addReviewsToContentValuesVector(movie.getReviews(), reviewVector);
         }
 
         ContentResolver resolver = getContext().getContentResolver();
@@ -195,27 +254,16 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
             movieIdVector.toArray(idValuesArray);
 
             if (sortKey.equalsIgnoreCase(getContext().getString(R.string.pref_sort_by_popular))) {
-                int deleted = resolver.delete(MovieContract.PopularEntry.CONTENT_URI, "1", null);
-                Log.d(LOG_TAG, "Removed " + deleted + " rows from popular movies table");
+//                int deleted = resolver.delete(MovieContract.PopularEntry.CONTENT_URI, "1", null);
+//                Log.d(LOG_TAG, "Removed " + deleted + " rows from popular movies table");
                 int inserted = resolver.bulkInsert(MovieContract.PopularEntry.CONTENT_URI, idValuesArray);
                 Log.d(LOG_TAG, "Sync finished, inserted " + inserted + " rows to popular movies table.");
             } else if (sortKey.equalsIgnoreCase(getContext().getString(R.string.pref_sort_by_top_rated))) {
-                int deleted = resolver.delete(MovieContract.TopRatedEntry.CONTENT_URI, "1", null);
-                Log.d(LOG_TAG, "Removed " + deleted + " rows from top rated movies table");
+//                int deleted = resolver.delete(MovieContract.TopRatedEntry.CONTENT_URI, "1", null);
+//                Log.d(LOG_TAG, "Removed " + deleted + " rows from top rated movies table");
                 int inserted = resolver.bulkInsert(MovieContract.TopRatedEntry.CONTENT_URI, idValuesArray);
                 Log.d(LOG_TAG, "Sync finished, inserted " + inserted + " rows to top rated movies table.");
             }
-
-            ContentValues trailersArray[] = new ContentValues[trailerVector.size()];
-            trailerVector.toArray(trailersArray);
-            int inserted = resolver.bulkInsert(MovieContract.TrailerEntry.CONTENT_URI, trailersArray);
-            Log.d(LOG_TAG, "Trailers sync finished, inserted " + inserted + " rows to trailers table.");
-
-
-            ContentValues reviewsArray[] = new ContentValues[reviewVector.size()];
-            reviewVector.toArray(reviewsArray);
-            inserted = resolver.bulkInsert(MovieContract.ReviewEntry.CONTENT_URI, reviewsArray);
-            Log.d(LOG_TAG, "Reviews sync finished, inserted " + inserted + " rows to reviews table.");
 
             // TODO: Remove movie rows that are do not match any entries in popular/toprated/favorite categories.
         }
@@ -239,15 +287,24 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         return newAccount;
     }
 
-    public static void syncNow(Context context) {
-        syncNow(getSyncAccount(context), context.getString(R.string.content_authority));
+    public static void syncListNow(Context context) {
+        syncNow(getSyncAccount(context), null, context.getString(R.string.content_authority));
     }
 
-    private static void syncNow(final Account account, final String authority) {
-        Bundle bundle = new Bundle();
-        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
-        bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
-        ContentResolver.requestSync(account, authority, bundle);
+    public static void syncDetailsNow(Context context, long movieId) {
+        Bundle extras = new Bundle();
+        extras.putLong(MOVIE_SYNC_EXTRAS_MOVIE_ID, movieId);
+        syncNow(getSyncAccount(context), extras, context.getString(R.string.content_authority));
+    }
+
+    private static void syncNow(final Account account, Bundle extras, final String authority) {
+        if (extras == null) {
+            extras = new Bundle();
+        }
+        extras.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        extras.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+
+        ContentResolver.requestSync(account, authority, extras);
     }
 
     private static void onAccountCreated(Account newAccount, Context context) {
@@ -265,13 +322,13 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         }
 
 
-        // Enable periodic sync
-        ContentResolver.setSyncAutomatically(newAccount, authority, true);
+        // TODO: Enable periodic sync ?
+        // ContentResolver.setSyncAutomatically(newAccount, authority, true);
 
         /*
          * Finally, let's do a sync to get things started
          */
-        syncNow(newAccount, authority);
+        syncNow(newAccount, null, authority);
     }
 
     public static void init(Context context) {
